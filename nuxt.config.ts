0aaf7c7d7a837291c40957bb1bcc2eb90c8e3782
franchardin <francisco.chardin@gmail.com>
2025-06-03 11:41:35 -0300

    modified deploy to gh-pages & added CNAME

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
+    target: 'static',
+  router: {
+    base: '/franchardin/kundera-ediciones-web_vue-nuxt'
+  },
   app: {
     baseURL: '/franchardin/kundera-ediciones-web_vue-nuxt', 
   },
